499078331f4cebfea8557ce9dbf0b60f6246e8b4
vladimir <vladimorkom@gmail.com>
2024-10-06 20:16:00 +1100

    Added telegram bot install code and token in config file

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-for i in range(6):
-  print(i)
+from config import token
+from telebot import TeleBot
+
+bot = TeleBot(token)
+
+@bot.message_handler(commands=['info'])
+def send_message(message):
+    bot.reply_to(message, 'Привет, меня зовут Владимир. Мне 14 лет')
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    bot.reply_to(message, 'What a photo!!!!!!!!')
+
+bot.infinity_polling()
+
+
